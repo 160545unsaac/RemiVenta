8561c8fba0a52dd9b86d82e2f5c7847b59229e2d
Lucio16 <46169347+Lucio16@users.noreply.github.com>
2019-01-20 11:34:37 -0500

    Add files via upload

---
+++ CDocVenta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibClases
+{
+    class CDocVenta : CEntidad
+    {
+        //===============ATRIBUTOS ====================
+        //---   Todos heredados de CEntidad        ----
+        //=============================================
+        //--------constructores -----------------------
+        public CDocVenta() : base("TDocVenta")
+        {
+        }
+        //-------Implementacion de Metodos Abstractos--
+        public override string[] NombresAtributos()
+        {
+            return new string[] { "NroDocVenta", "Fecha", "Tipo", "TipoPago", "CodCliente", "CodUsuario" };
+        }
+        public override int NroDeClaves()
+        {
+            return 1;///Falta mejores 
+        }
+    }
+}
---
+++ cDetalleVenta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibClases
+{
+    class CDetalleVenta : CEntidad
+    {
+        //===============ATRIBUTOS ====================
+        //---   Todos heredados de CEntidad        ----
+        //=============================================
+        //--------constructores -----------------------
+        public CDetalleVenta() : base("TDetalleVenta")
+        {
+        }
+        //-------Implementacion de Metodos Abstractos--
+        public override string[] NombresAtributos()
+        {
+            return new string[] { "NroDocVenta", "CodProducto", "Cantidad", "PrecioUnitario" };
+        }
+        public override int NroDeClaves()
+        {
+            return 2;///Falta mejores 
+        }
+    }
+}
